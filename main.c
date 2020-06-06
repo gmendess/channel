6bf396cdac9d6163cde27cc13a51042d11d0ced1
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-06-06 00:39:42 -0300

    Testes nas funcionalidades da fila

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "queue/queue.h"
 
 int main() {
+  
+  queue_t q;
+  queue_init(&q);
+  int x = 10;
+  int y = 20;
+
+  queue_push_back(&q, &x);
+  queue_push_back(&q, &y);
+  
+  void* popped_value;
+  queue_pop_front(&q, &popped_value);
+  printf("%d\n", *(int*) popped_value);
+
+  queue_pop_front(&q, &popped_value);
+  printf("%d\n", *(int*) popped_value);
+
+  queue_push_back(&q, &y);
+  queue_pop_front(&q, &popped_value);
+  printf("%d\n", *(int*) popped_value);
   
   return 0;
 }
