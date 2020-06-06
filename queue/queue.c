c6f932696d30763bc4a68be9800664128aa0f938
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-06-06 00:39:21 -0300

    Implementando funções básicas da fila, como criação, push back e pop front

--- queue/queue.c
+++ queue/queue.c
@@ -1,3 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "queue.h"
+
+int queue_init(queue_t* q) {
+  q->head = NULL;
+  q->tail = NULL;
+  q->length = 0;
+
+  return 0;
+}
+
+int queue_push_back(queue_t* q, void* value) {
+  QNODE_NEW(new_node, value);
+
+  if(q->length == 0)
+    q->head = new_node;
+  else
+    q->tail->next = new_node;
+  
+  q->tail = new_node;
+  ++q->length;
+
+  return 0;
+}
+
+int queue_pop_front(queue_t* q, void** ret) {
+  if(q->length == 0)
+    return 1;
+
+  qnode_t* first = q->head;
+  *ret = first->value;
+  q->head = first->next;
+  
+  free(first);
+  --q->length;
+  
+  return 0;
+}
--- queue/queue.h
+++ queue/queue.h
@@ -1,4 +1,27 @@
 #ifndef _CHANNEL_QUEUE_H
 #define _CHANNEL_QUEUE_H
 
+struct qnode {
+  void* value; // conteúdo armazenado pelo nó
+  struct qnode* next; // próximo nó
+};
+typedef struct qnode qnode_t;
+
+#define QNODE_NEW(name, value) \
+  qnode_t* name = malloc(sizeof(qnode_t)); \
+  name->value = value; \
+  name->next = NULL \
+ 
+
+struct queue {
+  qnode_t* head; // ponteiro pro primeiro nó da fila
+  qnode_t* tail; // ponteiro pro último nó da fila
+  size_t length; // número atual de nós
+};
+typedef struct queue queue_t;
+
+int queue_init(queue_t* q); // inicializa uma fila com os valores padrões
+int queue_push_back(queue_t* q, void* value); // adiciona nó no final da fila
+int queue_pop_front(queue_t* q, void** ret); // remove primeiro nó da fila
+
 #endif // _CHANNEL_QUEUE_H
