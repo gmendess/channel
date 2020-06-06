9585eba66371ec9cab68cfc6e147b96a9e040772
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-06-06 01:21:44 -0300

    Teste na função destroy

--- main.c
+++ main.c
@@ -3,25 +3,17 @@
 #include "queue/queue.h"
 
 int main() {
-  
+
   queue_t q;
   queue_init(&q);
+
   int x = 10;
   int y = 20;
 
   queue_push_back(&q, &x);
   queue_push_back(&q, &y);
-  
-  void* popped_value;
-  queue_pop_front(&q, &popped_value);
-  printf("%d\n", *(int*) popped_value);
 
-  queue_pop_front(&q, &popped_value);
-  printf("%d\n", *(int*) popped_value);
-
-  queue_push_back(&q, &y);
-  queue_pop_front(&q, &popped_value);
-  printf("%d\n", *(int*) popped_value);
+  queue_destroy(&q);
   
   return 0;
 }
