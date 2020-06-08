b8e32287642a8d89e33d092139f9ca3fddf34172
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-06-07 23:30:16 -0300

    Criando o tipo chan_cond_t, que é uma variável condicional com uma flag indicando se ela está em uso ou não

--- chan/chan.h
+++ chan/chan.h
@@ -7,10 +7,29 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+// Esse tipo contém uma pthread_cond_t e um booleano indicando se a variável condicional está ou não
+// em estado de waiting. A documentação diz que ao tentar destruir uma pthread_cond_t que ainda tem um
+// pthread_cond_wait pendente, o comportamente depende da implementação, podendo retornar EBUSY ou ficar
+// bloqueado até o sinal ser enviado (ref. https://linux.die.net/man/3/pthread_cond_destroy - vá em Errors). 
+// Como não posso depender da implementação, vou fazer isso eu mesmo
+struct chan_cond {
+  pthread_cond_t cond;
+  bool busy;
+};
+typedef struct chan_cond chan_cond_t;
+
+// syntax-sugar 
+// c = ponteiro para chan_cond_t
+// mutex = ponteiro para pthread_mutex_t
+#define chan_cond_wait(c, mutex) \
+  (*c).busy = true; \
+  pthread_cond_wait(c.cond, mutex); \
+  (*c).busy = false;
+
 struct chan {
   queue_t queue;
-  pthread_cond_t cond_read;
-  pthread_cond_t cond_write;
+  chan_cond_t cond_read;
+  chan_cond_t cond_write;
   pthread_mutex_t mutex;
   bool closed;
   size_t capacity;
