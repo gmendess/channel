9a92c34693f0ffc1f7a2e111c09a5adf4e7595ed
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-06-08 02:35:22 -0300

    teste no buffer do channel ao ler dados após ele ser fechado

--- main.c
+++ main.c
@@ -1,19 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "queue/queue.h"
+#include "chan/chan.h"
+
+chan_t ch;
+
+void* producer(void* args) {
+
+  while(1) {
+    int* value = malloc(sizeof(int));
+    
+    scanf("%d", value);
+    if(*value == 0) {
+      free(value);
+      break;
+    }
+    
+    chan_send(&ch, value);
+    puts("Produced!");
+  }
+
+  chan_close(&ch);
+
+  pthread_exit(0);
+}
+
+void* consumer(void* args) {
+  void* value = NULL;
+
+  while(chan_recv(&ch, &value) != ECLOSED) {
+    printf("thread %ld >>> %d\n", pthread_self(), *(int*) value);
+    free(value);
+  }
+  puts("channel closed!");
+
+  pthread_exit(0);
+}
 
 int main() {
 
-  queue_t q;
-  queue_init(&q);
-
-  int x = 10;
-  int y = 20;
-
-  queue_push_back(&q, &x);
-  queue_push_back(&q, &y);
-
-  queue_destroy(&q);
+  chan_init(&ch, 2);
+  pthread_t prod_id, con1_id, con2_id;
   
+  pthread_create(&prod_id, NULL, producer, NULL);
+  pthread_join(prod_id, NULL);
+
+  pthread_create(&con1_id, NULL, consumer, NULL);
+  pthread_create(&con2_id, NULL, consumer, NULL);
+
+  pthread_join(con1_id, NULL);
+  pthread_join(con2_id, NULL);
+
+  chan_destroy(&ch);
+
   return 0;
 }
