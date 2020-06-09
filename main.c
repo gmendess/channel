871905208c3545af5ad157194e508b6cb56c5354
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-06-09 01:42:59 -0300

    mais alguns testes, agora com envio de texto

--- main.c
+++ main.c
@@ -1,57 +1,59 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include "chan/chan.h"
 
-chan_t ch;
+void echo(void* _msg, void* args) {
+  char* msg = (char*) _msg;
+  int i = 0;
 
-void print_number(void* chan_value, void* args) {
-  printf("(%ld) %s: %d\n", pthread_self() % 10,(char*) args, *(int*) chan_value);
-  free(chan_value);
+  printf("Mensagem ecoada em maiúsculo: ");
+  while(msg[i]) {
+    putchar(toupper(msg[i]));
+    i++;
+  }
+  puts("\n");
 }
 
-void* producer(void* args) {
-  int* value = malloc(sizeof(int));
-  *value = 333;
-  chan_send(&ch, value);
- 
+void* recv_msg(void* args) {
+  chan_t* ch = (chan_t*) args;
+
+  chan_for_range(ch, echo, NULL);
+  puts("canal fechado, nada mais será ecoado!");
+
+  return NULL;
+}
+
+void* send_msg(void* args) {
+  chan_t* ch = (chan_t*) args;
+
+  char buf[256] = {0};  
   while(1) {
-    value = malloc(sizeof(int));
-    
-    scanf("%d", value);
-    if(*value == 0) {
-      free(value);
+    fgets(buf, sizeof(buf), stdin);
+    if(buf[0] == '\n')
       break;
-    }
-    
-    chan_send(&ch, value);
+
+    buf[strlen(buf) - 1] = '\0';
+    chan_send(ch, buf);
   }
 
-  chan_close(&ch);
-
-  pthread_exit(0);
-}
-
-void* consumer(void* args) {
-  void* value = NULL;
-
-  chan_for_range(&ch, print_number, "Valor impresso");
-  puts("channel closed!");
-
+  chan_close(ch);
   pthread_exit(0);
 }
 
 int main() {
 
+  chan_t ch;
+
   chan_init(&ch, 0);
-  pthread_t prod_id, con1_id, con2_id;
-  
-  pthread_create(&prod_id, NULL, producer, NULL);
-  pthread_create(&con1_id, NULL, consumer, NULL);
-  pthread_create(&con2_id, NULL, consumer, NULL);
+  pthread_t prod_id, con1_id;
+
+  pthread_create(&prod_id, NULL, send_msg, &ch);
+  pthread_create(&con1_id, NULL, recv_msg, &ch);
 
   pthread_join(prod_id, NULL);
   pthread_join(con1_id, NULL);
-  pthread_join(con2_id, NULL);
 
   chan_destroy(&ch);
 
