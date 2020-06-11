f4bbff7ad80aec06f62eb2a3593ae54375cdb69b
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-06-10 21:07:25 -0300

    Alguns testes mais pesados

--- main.c
+++ main.c
@@ -15,21 +15,57 @@ int main() {
   queue_t q;
   queue_init(&q, 3);
 
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
+
   queue_push_back(&q, &x);
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
+
+  queue_push_back(&q, &y);
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
+
+  queue_push_back(&q, &z);
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
+
+
+  queue_pop_front(&q, &retval);
+  printf("Valor: %d\n", *(int*) retval);
+
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
+
+  queue_pop_front(&q, &retval);
+  printf("Valor: %d\n", *(int*) retval);
+
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
+
+  queue_pop_front(&q, &retval);
+  printf("Valor: %d\n", *(int*) retval);
+
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
+
   queue_push_back(&q, &y);
   queue_push_back(&q, &z);
 
-  printf("size: %ld\n", q.length);
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
 
   queue_pop_front(&q, &retval);
-  printf("%d\n", *(int*) retval);
+  printf("Valor: %d\n", *(int*) retval);
 
-  queue_pop_front(&q, &retval);
-  printf("%d\n", *(int*) retval);
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
+
+  queue_push_back(&q, &x);
+
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
   
   queue_pop_front(&q, &retval);
-  printf("%d\n", *(int*) retval);
+  printf("Valor: %d\n", *(int*) retval);
 
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
+
+  queue_pop_front(&q, &retval);
+  printf("Valor: %d\n", *(int*) retval);
+
+  printf("\nsize: %ld | head: %ld | tail %ld\n", q.length, q.head, q.tail);
+  
   queue_destroy(&q);
 
   return 0;
